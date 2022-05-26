09d294960ff117c376f23e59aaf0a5fde028bf39
Gustavo Garay <102771062+GustavoGaray94@users.noreply.github.com>
2022-05-26 15:58:26 -0300

    Anotaciones

--- src/12.1-Clase-24-05-Ejercicio.ts
+++ src/12.1-Clase-24-05-Ejercicio.ts
@@ -4,12 +4,14 @@ Se debe permitir obtener el promedio anual (es decir, de sus tres notas) de un a
 (ingresado por el usuario)
 Luego de resolverlo, pensar en aprovechar métodos y discutir como representar la información */
 
+//segun el "cont" que va a representar cuantos alumnos a promediar
 function ingresarNombre(): void {
   for (let i: number = 0; i < cont; i++) {
     alumnos[i] = prompt("Ingrese el Nombre del Alumno: ");
   }
 }
 
+//multiplicamos el "cont" para así sacar la 3 notas por alumno ingresado
 function ingresarNotas(): void {
   for (let i: number = 0; i < cont * 3; i++) {
     notas[i] = Number(prompt("Ingrese el Nota del Alumno: "));
@@ -20,6 +22,7 @@ function promedio() {
   let c: number = 0;
   let promedio: number = 0;
 
+  //notas[c] y el agregado(++) para ir pasando por los indices
   for (let i: number = 0; i < cont; i++) {
     promedio = (notas[c] + notas[c++] + notas[c++]) / 3;
     console.log("El promedio del alumno", alumnos[i], " es: ", promedio);
